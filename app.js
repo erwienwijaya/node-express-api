96a93a19bcae5f1a4eb654c5c1df67378705b86f
Erwien Wijaya <erwin.cipta@gmail.com>
2021-11-26 13:24:01 +0700

    fix-cors

--- app.js
+++ app.js
@@ -15,9 +15,17 @@ let PORT = process.env.PORT
 let URL = 'http://localhost'
 let APP_STATUS = 'Production server'
 
-app.use(cors({
-    origin: `http://localhost:${PORT}`
-}));
+app.use(cors());
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 
 app.get('/', (req, res) => {
     res.send('Hi, this is Rest-API with Node.js');
